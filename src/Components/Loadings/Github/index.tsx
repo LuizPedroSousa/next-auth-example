aa199a1db1d997413ad3d619ebf7d7b7082a7a6a
LuizPedroSousa <luizpedrosousa64@gmail.com>
2021-04-11 01:30:18 -0300

    feat(pages/callback): added github callback page :sparkles:

---
+++ src/Components/Loadings/Github/index.tsx
@@ -0,0 +1,62 @@
+import React, { useEffect } from 'react'
+import Icon from '@chakra-ui/icon'
+import { Flex, Heading, VStack } from '@chakra-ui/layout'
+import { CircularProgress } from '@chakra-ui/progress'
+import { AiFillGithub } from 'react-icons/ai'
+import { SlideFade } from '@chakra-ui/transition'
+import { useAnimation } from 'framer-motion'
+import MotionCenter from '../../Motion/MotionCenter'
+
+const GithubLoading: React.FC = () => {
+  const githubAnimation = useAnimation()
+  const githubAnimationStart = async () => {
+    await githubAnimation.start('visible')
+    return await githubAnimation.start('slideUp')
+  }
+
+  useEffect(() => {
+    githubAnimationStart()
+  }, [])
+
+  const githubVariants = {
+    hidden: { opacity: 0, scale: 0, y: 0 },
+    visible: {
+      opacity: [null, 0.2],
+      scale: [null, 1.6, 1]
+    },
+    slideUp: {
+      transition: {
+        duration: 3,
+        type: 'spring',
+        repeat: Infinity,
+        repeatType: 'mirror'
+      },
+      y: [null, -20],
+      opacity: [null, 1]
+    }
+  }
+  return (
+    <SlideFade offsetY="200px" offsetX={0} in>
+      <Flex direction="column" align="center" justify="center">
+        <MotionCenter
+          initial="hidden"
+          variants={githubVariants as any}
+          animate={githubAnimation}
+          mb={8}
+          fontSize="6xl"
+          color="white"
+        >
+          <Icon as={AiFillGithub} />
+        </MotionCenter>
+        <VStack flexDir={['column', 'row']} spacing={[4, 0]}>
+          <Heading mr={[0, 4]} fontSize={['xl', '4xl']}>
+            Carregando informações...
+          </Heading>
+          <CircularProgress isIndeterminate color="blue.600" />
+        </VStack>
+      </Flex>
+    </SlideFade>
+  )
+}
+
+export default GithubLoading
---
+++ src/pages/callback/github.tsx
@@ -0,0 +1,64 @@
+import React from 'react'
+import { GetServerSideProps } from 'next'
+import withSession from '../../lib/middlewares/withSession'
+import Head from 'next/head'
+import Github from '../../Components/Loadings/Github'
+import axios from 'axios'
+import { Flex } from '@chakra-ui/layout'
+import UseAuth from '../../hooks/useAuth'
+interface GithubCallbackProps {
+  token: string
+}
+
+const GithubCallback: React.FC<GithubCallbackProps> = ({ token }) => {
+  UseAuth({ token, provider: 'github' })
+  return (
+    <Flex align="center" justify="center" flexDir="column" w="100%" h="100vh">
+      <Head>
+        <title>Nextauth | Callback | Github</title>
+      </Head>
+      <Github />
+    </Flex>
+  )
+}
+
+export default GithubCallback
+
+export const getServerSideProps: GetServerSideProps = withSession(
+  async ({ req, query: { code, userToken } }) => {
+    if (userToken) {
+      req.session.set('token', userToken)
+      await req.session.save()
+      return {
+        redirect: {
+          destination: '/',
+          permanent: false
+        }
+      }
+    }
+    if (!code) {
+      return {
+        redirect: {
+          destination: '/login',
+          permanent: false
+        }
+      }
+    }
+    try {
+      const { data } = await axios.post(
+        'http://localhost:3000/api/auth/callback/github',
+        {
+          code
+        }
+      )
+      return { props: { token: data.token } }
+    } catch {
+      return {
+        redirect: {
+          destination: '/login',
+          permanent: false
+        }
+      }
+    }
+  }
+)
