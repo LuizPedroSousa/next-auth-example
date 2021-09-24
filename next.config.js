e3bb42bfd1270c9d51d15b004954d2f616f8bc23
LuizPedroSousa <luizpedrosousa64@gmail.com>
2021-09-24 10:02:18 -0300

    fix(components/seo): fixing images thumb path :bug:

--- next.config.js
+++ next.config.js
@@ -1,4 +1,15 @@
+const withPlugins = require('next-compose-plugins')
 const withImages = require('next-images')
-module.exports = withImages({
-  esModule: true
+
+const imagePlugin = withImages({
+    esModule: true,
 })
+
+const nextConfig = {
+    env: {
+        NEXT_PUBLIC_URL: process.env.NEXT_PUBLIC_URL,
+        VERCEL_URL: process.env.VERCEL_URL,
+    }
+}
+
+module.exports = withPlugins([imagePlugin, nextConfig])
--- src/components/Seo.tsx
+++ src/components/Seo.tsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import Head from 'next/head'
 import { useRouter } from 'next/router'
+import { theme } from '@chakra-ui/react'
 
 interface SeoProps {
   title: string
@@ -15,30 +16,49 @@ const Seo: React.FC<SeoProps> = ({ title, description, thumb }) => {
       <title>{title} | Next Auth</title>
       <meta name="title" content={`${title} | Next Auth`} />
       <meta name="description" content={description} />
+      <meta name="theme-color" content={theme.colors.blue[400]} />
+      <meta
+        name="image"
+        content={`https://${process.env.VERCEL_URL}/images/thumbs${thumb}`}
+      />
+      <meta property="image:alt" content={`${title} | Next Auth`} />
 
+      <meta property="og:title" content={`${title} | Next Auth`} />
+      <meta property="og:locale" content="pt_BR" />
+      <meta property="og:site_name" content="Next Auth" />
+      <meta property="og:image:width" content="1200" />
+      <meta property="og:image:width" content="627" />
       <meta property="og:type" content="website" />
       <meta
         property="og:url"
-        content={process.env.NEXT_PUBLIC_URL + router.asPath}
+        content={`https://${process.env.VERCEL_URL}${router.asPath}`}
       />
-      <meta property="og:title" content={`${title} | Next Auth`} />
       <meta property="og:description" content={description} />
       <meta
         property="og:image"
-        content={`${process.env.NEXT_PUBLIC_URL}/images/thumbs${thumb}`}
+        content={`https://${process.env.VERCEL_URL}/images/thumbs${thumb}`}
       />
+      <meta property="og:image:type" content="image/png" />
+      <meta property="og:image:alt" content={`${title} | Next Auth`} />
 
-      <meta property="twitter:card" content="summary_large_image" />
+      <meta name="twitter:title" content={`${title} | Next Auth`} />
+      <meta name="twitter:card" content="summary_large_image" />
       <meta
-        property="twitter:url"
-        content={process.env.NEXT_PUBLIC_URL + router.asPath}
+        name="twitter:url"
+        content={`https://${process.env.VERCEL_URL}${router.asPath}`}
       />
-      <meta property="twitter:title" content={`${title} | Next Auth`} />
-      <meta property="twitter:description" content={description} />
+      <meta name="twitter:description" content={description} />
       <meta
-        property="twitter:image"
-        content={`${process.env.NEXT_PUBLIC_URL}/images/thumbs${thumb}`}
+        name="twitter:image"
+        content={`https://${process.env.VERCEL_URL}/images/thumbs${thumb}`}
       />
+      <meta
+        name="twitter:image:src"
+        content={`https://${process.env.VERCEL_URL}/images/thumbs${thumb}`}
+      />
+      <meta name="twitter:image:alt" content={`${title} | Next Auth`} />
+      <meta name="twitter:image:width" content="1200" />
+      <meta name="twitter:image:width" content="627" />
     </Head>
   )
 }
