67769f7bdf92ec056998c2c2b0a90b2f4fdab896
LuizPedroSousa <luizpedrosousa64@gmail.com>
2023-09-27 09:47:51 -0300

    chore(pages/index): add redirect to login :lipstick:

---
+++ src/pages/index.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import { GetServerSideProps } from 'next'
+
+export default function Home() {
+  return <div></div>
+}
+
+export const getServerSideProps: GetServerSideProps = async () => {
+  return {
+    redirect: {
+      destination: '/login',
+      permanent: false
+    }
+  }
+}
